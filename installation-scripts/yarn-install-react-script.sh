324f60e83b4f728470a0fca749e78ef986537180
Apollo Tang <tang.apollo@gmail.com>
2019-07-24 01:49:16 -0400

    add installation script

---
+++ installation-scripts/yarn-install-react-script.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+
+
+yarn add -D \
+    @babel/cli \
+    @babel/core \
+    #
+    @babel/plugin-proposal-class-properties \
+    #
+    @babel/preset-env \
+    @babel/preset-react \
+    @babel/preset-typescript \
+    #
+    @types/node \
+    @types/react \
+    @types/react-dom \
+    #
+    babel-eslint \
+    babel-loader \
+    #
+    eslint \
+    eslint-plugin-react \
+    eslint-plugin-react-hooks \
+    #
+    html-webpack-plugin \
+    #
+    webpack \
+    webpack-cli \
+    webpack-dev-server
+
+
+# yarn add -D \
+#     style-loader \
+#     css-loader \
+
+
+yarn add \
+    @babel/polyfill \
+    react \
+    react-dom
+
+
+yarn add \
+    @emotion/core \
+    @emotion/styled \
+    babel-plugin-emotion \
+
+
