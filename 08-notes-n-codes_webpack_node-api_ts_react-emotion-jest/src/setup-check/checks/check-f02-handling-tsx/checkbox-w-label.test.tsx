ba1b89c9f14281c1b28ed08ff85f0f8ce34b8ce2
Apollo Tang <tang.apollo@gmail.com>
2019-08-18 03:41:23 -0400

    fix type

--- 08-notes-n-codes_webpack_node-api_ts_react-emotion-jest/src/setup-check/checks/check-f02-handling-tsx/checkbox-w-label.test.tsx
+++ 08-notes-n-codes_webpack_node-api_ts_react-emotion-jest/src/setup-check/checks/check-f02-handling-tsx/checkbox-w-label.test.tsx
@@ -3,23 +3,25 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as TestUtils from 'react-dom/test-utils';
+type C = React.Component
 
 const CheckboxWithLabel = require('./checkbox-w-label').default;
 
+
 it('CheckboxWithLabel changes the text after click', () => {
   // Render a checkbox with label in the document
-  const checkbox = TestUtils.renderIntoDocument(
+  const checkbox = TestUtils.renderIntoDocument<C>(
     <CheckboxWithLabel labelOn="On" labelOff="Off" />,
-  );
+  ) as C;
 
-  const checkboxNode = ReactDOM.findDOMNode(checkbox);
+  const checkboxNode = ReactDOM.findDOMNode(checkbox) as HTMLElement;
 
   // Verify that it's Off by default
   expect(checkboxNode.textContent).toEqual('Off');
 
   // Simulate a click and verify that it is now On
   TestUtils.Simulate.change(
-    TestUtils.findRenderedDOMComponentWithTag(checkbox, 'input'),
+      TestUtils.findRenderedDOMComponentWithTag(checkbox, 'input'),
   );
   expect(checkboxNode.textContent).toEqual('On');
 });
