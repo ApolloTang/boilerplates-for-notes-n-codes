9f03cb458ce87935bd8ab019c6d05943f8d5c991
Apollo Tang <tang.apollo@gmail.com>
2019-08-31 21:20:20 -0400

    [08] update eslintrc and gitignore

--- 08-notes-n-codes_webpack_node-api_ts_react-emotion-jest/.eslintrc.js
+++ 08-notes-n-codes_webpack_node-api_ts_react-emotion-jest/.eslintrc.js
@@ -8,7 +8,9 @@ module.exports = {
     "extends": [
       "eslint:recommended",
       // "airbnb",
-      "plugin:jest/recommended"
+      "plugin:jest/recommended",
+      "plugin:@typescript-eslint/eslint-recommended",
+      "plugin:@typescript-eslint/recommended",
     ],
     "globals": {
         "Atomics": "readonly",
@@ -28,19 +30,29 @@ module.exports = {
       "emotion"
     ],
     "rules": {
+      "semi": [1, "never"],
+      "quotes": ["warn", "single"],
+      "indent": ["off", 2],
+      "no-unused-vars": "warn",
+
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/explicit-function-return-type": "off", // without this you will get incosistence behaviour compare to barebone @typescript-eslint
+      "@typescript-eslint/no-use-before-define": "off",
+      "@typescript-eslint/member-delimiter-style": ["warn", {
+        "multiline":{"delimiter":"none","requireLast":true}, "singleline":{"delimiter":"semi","requireLast":false} // single line config is not working
+      }],
+      "@typescript-eslint/type-annotation-spacing": "off",
+      "@typescript-eslint/camelcase": "off",
+      "@typescript-eslint/no-var-requires": "off",
+
       "react-hooks/rules-of-hooks": "error",
       "react/jsx-uses-react": "error",        // https://github.com/eslint/eslint/issues/11183
       "react/jsx-uses-vars": ["error"],       // https://github.com/eslint/eslint/issues/8226
 
-      // "emotion/jsx-import": "error",       // not need because we are using babel plugin
-      // "emotion/no-vanilla": "error",       // not need because we are using react
+      // "emotion/jsx-import": "error",       // not needed because we are using babel plugin
+      // "emotion/no-vanilla": "error",       // not needed because we are using react
       "emotion/import-from-emotion": "error",
       "emotion/styled-import": "error",
-
-      "semi": ["error", "never"],
-      "quotes": [1, "single"],
-      "indent": ["off", 2],
-      "no-unused-vars": 1
     },
     "overrides": [
       {
