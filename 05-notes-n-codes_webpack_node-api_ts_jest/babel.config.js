f5899d3cfe012c0c8897278a73946f9409738841
Apollo Tang <tang.apollo@gmail.com>
2019-10-25 20:34:01 -0400

    [05-notes-n-codes_webpack_node-api_ts_jest] migrage babel/polyfill to core-js@2

--- 05-notes-n-codes_webpack_node-api_ts_jest/babel.config.js
+++ 05-notes-n-codes_webpack_node-api_ts_jest/babel.config.js
@@ -7,7 +7,13 @@ module.exports = function (api) {
     [
       '@babel/preset-env',
       {
+        'debug': false,
+        'targets': {'ie':'11'},
         'useBuiltIns': 'usage',
+        // 'corejs': 3,  // dynamic import is broken:
+                         // https://github.com/babel/babel/issues/9872
+                         // so we will use corejs@2 instead
+        'corejs': 2,
         'modules': isTest ? 'commonjs' : false
       }
     ],
