91d2dfd79c809364d01edf5d73a7f4cd26dd9dcf
Apollo Tang <tang.apollo@gmail.com>
2019-10-28 01:24:07 -0400

    [node 02] fix tsc error

--- node/02-node-notes-n-codes_node_ts_jest/src/main.ts
+++
@@ -1,9 +0,0 @@
-const textNode = document.createTextNode('div#app')
-const container = document.createElement('div')
-container.id = 'app'
-container.appendChild(textNode)
-
-document.body.appendChild(container)
-
-console.log('this is main.js')
-
