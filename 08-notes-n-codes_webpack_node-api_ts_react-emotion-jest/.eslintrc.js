6a6a9c0f3d6c2c5cd68808ae5540948380a58878
Apollo Tang <tang.apollo@gmail.com>
2020-03-16 22:22:40 -0400

    update eslintrc

--- 08-notes-n-codes_webpack_node-api_ts_react-emotion-jest/.eslintrc.js
+++ 08-notes-n-codes_webpack_node-api_ts_react-emotion-jest/.eslintrc.js
@@ -1,70 +1,83 @@
 module.exports = {
-    "parser": "@typescript-eslint/parser",
-    "env": {
-        "browser": true,
-        "node": true, // https://stackoverflow.com/questions/49789177/module-is-not-defined-and-process-is-not-defined-in-eslint-in-visual-studio-code
-        "es6": true
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": 2019,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true,
     },
-    "extends": [
-      "eslint:recommended",
-      // "airbnb",
-      "plugin:jest/recommended",
-      "plugin:@typescript-eslint/eslint-recommended",
-      "plugin:@typescript-eslint/recommended",
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-      "react",
-      "react-hooks",
-      "@typescript-eslint",
-      "emotion"
-    ],
-    "rules": {
-      "semi": [1, "never"],
-      "quotes": ["warn", "single"],
-      "indent": ["off", 2],
-      "no-unused-vars": "warn",
+  },
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly",
+  },
+  "env": {
+    "browser": true,
+    "node": true, // https://stackoverflow.com/questions/49789177/module-is-not-defined-and-process-is-not-defined-in-eslint-in-visual-studio-code
+    "es6": true,
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:jest/recommended",
+  ],
+  "plugins": [
+    "react",
+    "react-hooks",
+    "emotion",
+  ],
+  "rules": {
+    "semi": ["warn", "never"],
+    "quotes": ["warn", "single"],
+    "indent": ["off", 2],
+    "no-unused-vars": "warn",
+    "strict": ["error", "never"],
 
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/explicit-function-return-type": "off", // without this you will get incosistence behaviour compare to barebone @typescript-eslint
-      "@typescript-eslint/no-use-before-define": "off",
-      "@typescript-eslint/member-delimiter-style": ["warn", {
-        "multiline":{"delimiter":"none","requireLast":true}, "singleline":{"delimiter":"semi","requireLast":false} // single line config is not working
-      }],
-      "@typescript-eslint/type-annotation-spacing": "off",
-      "@typescript-eslint/camelcase": "off",
-      "@typescript-eslint/no-var-requires": "off",
-      "@typescript-eslint/class-name-casing": ["off", {
-        "allowUnderscorePrefix":true
-      }],
-      "@typescript-eslint/ban-ts-ignore": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react/jsx-uses-react": "error",        // https://github.com/eslint/eslint/issues/11183
+    "react/jsx-uses-vars": ["error"],       // https://github.com/eslint/eslint/issues/8226
 
-      "react-hooks/rules-of-hooks": "error",
-      "react/jsx-uses-react": "error",        // https://github.com/eslint/eslint/issues/11183
-      "react/jsx-uses-vars": ["error"],       // https://github.com/eslint/eslint/issues/8226
+    "jest/expect-expect": "off"
+    // "emotion/jsx-import": "error",       // not needed because we are using babel plugin
+    // "emotion/no-vanilla": "error",       // not needed because we are using react
+    "emotion/import-from-emotion": "error",
+    "emotion/styled-import": "error",
+  },
+  "overrides": [
+    {
+      "files": "**/*.+(ts|tsx)",
+      "parser": "@typescript-eslint/parser",
+      "parserOptions": {
+        "tsconfigRootDir": __dirname,
+        "project": "./tsconfig.json"
+      },
+      "plugins": [
+        "@typescript-eslint/eslint-plugin",
+      ],
+      "extends": [
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        'plugin:@typescript-eslint/recommended-requiring-type-checking'
+      ],
+      "rules": {
+        "no-undef": "off", // https://github.com/eslint/typescript-eslint-parser/issues/437
 
-      // "emotion/jsx-import": "error",       // not needed because we are using babel plugin
-      // "emotion/no-vanilla": "error",       // not needed because we are using react
-      "emotion/import-from-emotion": "error",
-      "emotion/styled-import": "error",
-    },
-    "overrides": [
-      {
-        "files": ["**/*.ts", "**/*.tsx"],
-        "parser": "@typescript-eslint/parser",
-        "rules": {
-          "no-undef": "off" // https://github.com/eslint/typescript-eslint-parser/issues/437
-        }
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/explicit-function-return-type": "off", // without this you will get incosistence behaviour compare to barebone @typescript-eslint
+        "@typescript-eslint/no-use-before-define": "off",
+        "@typescript-eslint/member-delimiter-style": ["warn", {
+          "multiline":{"delimiter":"none","requireLast":true}, "singleline":{"delimiter":"semi","requireLast":false} // single line config is not working
+        }],
+        "@typescript-eslint/type-annotation-spacing": "off",
+        "@typescript-eslint/camelcase": "off",
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/class-name-casing": ["off", {
+          "allowUnderscorePrefix":true
+        }],
+        "@typescript-eslint/ban-ts-ignore": "off"
+        "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/no-misused-promises": [ "error", { "checksVoidReturn": false } ]
       }
-    ]
-};
+    }
+  ]
+}
+
