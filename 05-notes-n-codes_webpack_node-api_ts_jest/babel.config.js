66d7f2cfeb827d66c068a9cef89349a48c8b7327
Apollo Tang <tang.apollo@gmail.com>
2020-07-23 23:36:59 -0400

    upgrade b08

--- 05-notes-n-codes_webpack_node-api_ts_jest/babel.config.js
+++ 05-notes-n-codes_webpack_node-api_ts_jest/babel.config.js
@@ -13,7 +13,7 @@ module.exports = function (api) {
         // 'corejs': 3,  // dynamic import is broken:
                          // https://github.com/babel/babel/issues/9872
                          // so we will use corejs@2 instead
-        'corejs': 2,
+        'corejs': 3,
         'modules': isTest ? 'commonjs' : false
       }
     ],
