89d788ba01f714d900f6570e076026337037f83b
Apollo Tang <tang.apollo@gmail.com>
2022-03-24 01:42:33 -0400

    b08 fix enzyme broken for react 17

--- 08-notes-n-codes_webpack_node-api_ts_react-emotion-jest/test-setup/jest.init.js
+++ 08-notes-n-codes_webpack_node-api_ts_react-emotion-jest/test-setup/jest.init.js
@@ -1,7 +1,7 @@
 // This file will run on each test, before jest is loaded
 
 const Enzyme = require('enzyme');
-const Adapter = require('enzyme-adapter-react-16');
+const Adapter = require('@wojtekmaj/enzyme-adapter-react-17'); // https://github.com/enzymejs/enzyme/issues/2429#issuecomment-679265564
 
 Enzyme.configure({ adapter: new Adapter() });
 
