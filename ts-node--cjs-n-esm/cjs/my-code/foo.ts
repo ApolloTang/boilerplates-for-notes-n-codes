2e4a26aa94aa682775614b7b7d5d603038374bf5
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2023-01-08 06:03:40 -0500

    i[ts-node--cjs-n-esm] fix: unable to consume ESM in cjs

--- ts-node--cjs-n-esm/cjs/my-code/foo.ts
+++ ts-node--cjs-n-esm/cjs/my-code/foo.ts
@@ -1,3 +1,21 @@
-const assert = require("node:assert").strict;
+// ---------------------------
+// Demo for consuming Pure ESM
+// ---------------------------
+declare module 'chalk'
 
-assert.deepEqual([[[1, 2, 3]], 4, 5], [[[1, 2, "3"]], 4, 5]);
+// const c = require('chalk')   // <--- This will not work because chalk@5 is a pure ESM package.
+// Below is solution from
+//   https://stackoverflow.com/questions/70309135/chalk-error-err-require-esm-require-of-es-module
+async function chalk() {
+  return (await import("chalk")).default;
+}
+async function main(){
+  console.log((await chalk()).red(">>>>"));
+}
+main()
+
+// ------------------------
+// Demo for using assertion
+// ------------------------
+const assert = require("node:assert").strict
+assert.deepEqual([[[1, 2, 3]], 4, 5], [[[1, 2, "3"]], 4, 5])
