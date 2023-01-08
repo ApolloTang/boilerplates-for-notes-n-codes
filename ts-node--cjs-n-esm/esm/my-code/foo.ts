dad890d86047e39c06a230b1407bf474ccccf783
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2023-01-08 06:47:10 -0500

    [ts-node--cjs-n-esm/esm] add example of consuming pure ESM package

--- ts-node--cjs-n-esm/esm/my-code/foo.ts
+++ ts-node--cjs-n-esm/esm/my-code/foo.ts
@@ -1,3 +1,14 @@
+import chalk from  'chalk'
 import { strict as assert } from 'node:assert';
 
-assert.deepEqual([[[1, 2, 3]], 4, 5], [[[1, 2, "3"]], 4, 5]);
+// ---------------------------
+// Demo for consuming Pure ESM
+// ---------------------------
+console.log(chalk.red('>>>>'))
+
+// ------------------------
+// Demo for using assertion
+// ------------------------
+//assert.deepEqual([[[1, 2, 3]], 4, 5], [[[1, 2, "3"]], 4, 5]);
+assert.deepEqual([[[1, 2, 3]], 4, 5], [[[1, 2, 3]], 4, 5]);
+
