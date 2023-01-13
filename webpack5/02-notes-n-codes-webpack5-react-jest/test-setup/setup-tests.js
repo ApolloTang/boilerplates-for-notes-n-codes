236e2260b1a6d8d93e3a8580c5eebf0ed41036c7
ApolloTang <1150961+ApolloTang@users.noreply.github.com>
2023-01-12 22:35:09 -0500

    [02-notes-n-codes-webpack5-react-jest] added whatwg-fetch

--- webpack5/02-notes-n-codes-webpack5-react-jest/test-setup/setup-tests.js
+++ webpack5/02-notes-n-codes-webpack5-react-jest/test-setup/setup-tests.js
@@ -1,5 +1,10 @@
 // This file will run on each test, after jest is loaded
 
+import 'whatwg-fetch' // Polyfill "window.fetch" used in the React component.
+                      // See:
+                      //   https://github.com/mswjs/msw/issues/686
+                      //   https://github.com/mswjs/examples/blob/master/examples/with-jest/jest.setup.js
+
 import '@testing-library/jest-dom/extend-expect'
 
 
